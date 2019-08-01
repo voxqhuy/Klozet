c16d678c1fca308ee2b31dc07902642b9bb3997a
Vo Huy <voxqhuy@gmail.com>
2019-08-01 16:06:26 -0400

    Create the project

---
+++ Klozet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Klozet
+//
+//  Created by Developers on 8/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

e95b740dd3403e5b12d4e440a36e169f5e23066c
Vo Huy <voxqhuy@gmail.com>
2019-08-01 17:06:26 -0400

    re structure the project

--- Klozet/Controllers/HomeViewController.swift
+++ Klozet/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Klozet
 //
 //  Created by Developers on 8/1/19.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 

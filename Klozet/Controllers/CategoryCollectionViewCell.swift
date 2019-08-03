3d95f10ed5135a1bd26e7de0df275e2bb545b084
Huy Vo <voxqhuy@gmail.com>
2019-08-03 16:35:44 -0400

    create categoryCollectionViewCell + create itemCategoryVC + create item storyboard

---
+++ Klozet/Controllers/CategoryCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Klozet
+//
+//  Created by Huy Vo on 8/3/19.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

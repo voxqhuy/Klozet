95a2c06f9b2cac4fa0f13cf393e3c1e829d828eb
Huy Vo <voxqhuy@gmail.com>
2019-08-03 19:53:33 -0400

    add MyString class

---
+++ Klozet/Common/Constants/MyString.swift
@@ -0,0 +1,25 @@
+//
+//  MyString.swift
+//  Klozet
+//
+//  Created by Huy Vo on 8/3/19.
+//
+
+import Foundation
+
+struct MyString {
+    // MARK: - Data Source
+    let itemCategoryName = [
+        "Top",
+        "Bottom",
+        "Shoes",
+        "Accessories"
+    ]
+    
+    let itemCategoryDescription = [
+        "shirts, jackets, sweaters, ...",
+        "shorts, pants, skirts, ...",
+        "sneakers, boots, sandals, ...",
+        "hats, watches, bracelets, ..."
+    ]
+}

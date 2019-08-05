5ada0394c93091c98d9819cbefb5257cc4f3ca38
Vo Huy <voxqhuy@gmail.com>
2019-08-05 10:20:46 -0400

    add GridCollectionViewCell

---
+++ Klozet/Views/Shared/GridCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GridCollectionViewCell.swift
+//  Klozet
+//
+//  Created by Developers on 8/5/19.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
